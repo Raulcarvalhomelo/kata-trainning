321f2aa5e6be34b72d46e5ff38016a0aed0a22c5
raulcarvalhomelo <raulcarvalhomelo@gmail.com>
2023-12-31 11:36:51 -0300

    atttsblackjack

--- ts/RockPaperScissor.ts
+++ ts/RockPaperScissor.ts
@@ -1,4 +1,13 @@
 export function rps(p1: string, p2: string): string{
-    
-  return ""
+  const rules: {[index:string]:string} = {
+    'rock': 'scissors',
+    'paper': 'rock',
+    'scissors': 'paper'
   }
+  if (p1 === p2) return 'Draw!'
+  
+  if (rules[p1] == p2) return 'Player 1 won!'
+  return 'Player 2 won!'
+}
+
+console.log(rps("rock", "scissors"))
